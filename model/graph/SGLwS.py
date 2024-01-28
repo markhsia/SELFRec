00674ccc043ae7eef210b706319363851ca8920e
CubicPower <55009831+markhsia@users.noreply.github.com>
2024-01-28 11:30:28 +0800

    Update SGLwS.py

--- model/graph/SGLwS.py
+++ model/graph/SGLwS.py
@@ -42,8 +42,8 @@ class SGLwS(GraphRecommender):
                 if epoch<=9:
                     batch_loss =  rec_loss + 0* l2_reg_loss(self.reg, user_emb, pos_item_emb) + cl_loss
                 else:
-                    alpha = 0.5 + (target - 0.5) * (epoch-9) / 11.0 
-                    batch_loss =  alpha * rec_loss + 0 * l2_reg_loss(self.reg, user_emb, pos_item_emb) + (1 - alpha) * cl_loss
+                    alpha =  0.5 * (epoch-9) / 11.0 
+                    batch_loss =  (1 - alpha) * rec_loss + 0 * l2_reg_loss(self.reg, user_emb, pos_item_emb) + (1 + alpha) * cl_loss
                     
                 # Backward and optimize
                 optimizer.zero_grad()
