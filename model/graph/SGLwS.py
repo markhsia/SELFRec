86a383e8e58ab861f30039cdc04d56ea6dc491f4
CubicPower <55009831+markhsia@users.noreply.github.com>
2024-04-07 22:15:25 +0800

    Update SGLwS.py

--- model/graph/SGLwS.py
+++ model/graph/SGLwS.py
@@ -49,12 +49,14 @@ class SGLwS(GraphRecommender):
                     #batch_loss =  rec_loss +  cl_loss + (cl_loss/rec_loss - 5.5)**2
                 """
 
-                alpha = 1.0 * math.sin(2*math.pi * epoch / 10.0) 
+                #alpha = 1.0 * math.sin(2*math.pi * epoch / 10.0) 
                 #alpha = 0.5 * math.sin(2*math.pi * epoch / 10.0) 
                 #alpha = 0.5 * math.cos(2*math.pi * epoch / 10.0)
 
-                c = np.random.normal(0,1, (1))
-                alpha = c[0]
+                #c = np.random.normal(0,1, (1))
+                #alpha = c[0]
+
+                alpha = 1.0 * math.cos(2*math.pi * epoch / 10.0)
                 batch_loss =  (1 - alpha) * rec_loss + (1 + alpha) * cl_loss 
                 
                 # Backward and optimize
