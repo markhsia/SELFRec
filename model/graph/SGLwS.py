9c412438db8b821e226efee917e29b3d1f6db5f9
CubicPower <55009831+markhsia@users.noreply.github.com>
2024-04-14 23:34:18 +0800

    Update SGLwS.py

--- model/graph/SGLwS.py
+++ model/graph/SGLwS.py
@@ -60,9 +60,9 @@ class SGLwS(GraphRecommender):
 
                 p=4
                 # square wave
-                alpha=-1
+                alpha=-0.5
                 if (epoch%p)<p/2:
-                    alpha=1
+                    alpha=0.5
                 batch_loss =  (1 - alpha) * rec_loss + (1 + alpha) * cl_loss 
                 
                 # Backward and optimize
